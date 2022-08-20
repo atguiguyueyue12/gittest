5ce7fd55e41f4442a697227b35616e7f2c60c922
Layne <Layne@atguigu.com>
2022-08-20 21:56:57 +0800

    first commit

---
+++ src/main/java/com/at/GitTest.java
@@ -0,0 +1,15 @@
+package com.at;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+/**
+ * @author shkstart
+ * @create 2022-08-20 21:40
+ */
+public class GitTest {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello,git!");
+    }
+}
